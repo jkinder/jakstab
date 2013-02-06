94074f0e105acc47b328e99c0f411f6017378551
Johannes Kinder <johannes.kinder@epfl.ch>
2013-02-06 15:22:36 +0100

    Build ASM CFG with proper graph search.

--- src/org/jakstab/cfa/AsmCFG.java
+++ src/org/jakstab/cfa/AsmCFG.java
@@ -1,12 +1,14 @@
 package org.jakstab.cfa;
 
+import java.util.Deque;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
 
 import org.jakstab.Program;
+import org.jakstab.asm.BranchInstruction;
 import org.jakstab.asm.Instruction;
-import org.jakstab.rtl.expressions.ExpressionFactory;
 import org.jakstab.rtl.statements.RTLAssume;
 import org.jakstab.rtl.statements.RTLGoto;
 import org.jakstab.rtl.statements.RTLStatement;
@@ -22,49 +24,70 @@ public class AsmCFG {
 	
 	public AsmCFG(ControlFlowGraph cfg) {
 		
+		/*
+		 * When constructing an ASM CFG from a CFA, we have the following problem:
+		 * The VPC can change within an instruction, so if we just skip intra-instruction
+		 * edges, the VPC value for CFA edge entering the instruction can be different from
+		 * the one for the CFA edge leaving the instruction. In that case, the instruction 
+		 * label is different, and the edges don't connect.
+		 * 
+		 * Therefore, we define the VPC of an instruction location to be the VPC at the 
+		 * instruction entry.
+		 * 
+		 * We can't just iterate over all VPC locations and check whether an VPC, address pair
+		 * already has an instruction, because this might put an instruction twice if the VPC
+		 * changes in the middle of it.  
+		 */
+		
 		nodes = new HashMap<Location, Instruction>();
 		edges = HashMultimap.create();
 		
 		Program p = Program.getProgram();
-		for (Location l : cfg.getNodes()) {
-			// the instruction inserted here might be null (if it's a stub/prologue location)
-			Location iLoc = instructionLoc(l);
-			if (!nodes.containsKey(iLoc))
-				nodes.put(iLoc, p.getInstruction(l.getAddress()));
-		}
 		
-		for (CFAEdge e : cfg.getEdges()) {
-			// If this is a forward intra-instruction-edge, skip it
-			if (e.getSource().getAddress().equals(e.getTarget().getAddress()) && 
-					e.getSource().getLabel().getIndex() < e.getTarget().getLabel().getIndex())
+		Deque<Location> worklist = new LinkedList<Location>();
+		worklist.add(cfg.getEntryPoint());
+		
+		while (!worklist.isEmpty()) {
+			Location instrEntry = worklist.pop();
+			if (nodes.containsKey(instrEntry))
 				continue;
+			// the instruction might be null (if it's a stub/prologue location)
+			Instruction instr = p.getInstruction(instrEntry.getAddress());
+			nodes.put(instrEntry, instr);
 			
-			RTLStatement stmt = (RTLStatement)e.getTransformer();
-			String label = "";
+			Set<CFAEdge> outEdges;
+			Location curLoc = instrEntry;
+			do {
+				outEdges = cfg.getOutEdges(curLoc);
+				if (outEdges.size() != 1)
+					break;
+				curLoc = outEdges.iterator().next().getTarget();				
+			} while (curLoc.getAddress().equals(instrEntry.getAddress()) && 
+					curLoc.getLabel().getIndex() >= instrEntry.getLabel().getIndex());
 			
-			if (stmt instanceof RTLAssume) {
-				// Get the original goto from the program (not the converted assume) 
-				RTLGoto rtlGoto = ((RTLAssume) stmt).getSource();
+			for (CFAEdge e : outEdges) {
+				RTLStatement stmt = (RTLStatement)e.getTransformer();
+				String label = "";
+				
+				if (instr instanceof BranchInstruction) {
+					assert stmt instanceof RTLAssume;
+					BranchInstruction bi = (BranchInstruction)instr;
+					if (bi.isConditional()) {
+						// Get the original goto from the program (not the converted assume) 
+						RTLGoto rtlGoto = ((RTLAssume) stmt).getSource();
 
-				// If this is the fall-through edge, output F, otherwise T
-				// If the assume in the edge has the same nextlabel as Goto, then it's the fall-through
-				if (rtlGoto != null && !rtlGoto.getCondition().equals(ExpressionFactory.TRUE))
-					label = stmt.getNextLabel().equals(rtlGoto.getNextLabel()) ? "F" : "T";
+						// If this is the fall-through edge, output F, otherwise T
+						// If the assume in the edge has the same nextlabel as Goto, then it's the fall-through
+						label = stmt.getNextLabel().equals(rtlGoto.getNextLabel()) ? "F" : "T";
+					}
+				}
+
+				edges.put(instrEntry, Pair.create(e.getTarget(), (Object)label));
+				worklist.add(e.getTarget());
 			}
+				
+		}
 
-			edges.put(instructionLoc(e.getSource()), Pair.create(instructionLoc(e.getTarget()), (Object)label));
-		}
-	}
-	
-	private Location instructionLoc(Location l) {
-		if (l.getLabel().getIndex() == 0)
-			return l;
-		if (l instanceof VpcLocation) {
-			return new VpcLocation(((VpcLocation) l).getVPC(), new RTLLabel(l.getAddress(), 0));
-		} else if (l instanceof RTLLabel) {
-			return new RTLLabel(l.getAddress(), 0); 
-		}
-		throw new IllegalArgumentException("Unsupported location type");
 	}
 	
 	public Instruction getInstruction(Location l) {
